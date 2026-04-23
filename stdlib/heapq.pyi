c80a55c993fe4ef2850be5392fcf62a1e10151db
Jonathan Dung <jonathandung@yahoo.com>
2026-04-23 11:20:03 +0800

    undo

--- stdlib/heapq.pyi
+++ stdlib/heapq.pyi
@@ -1,8 +1,8 @@
 import sys
 from _heapq import *
-from _typeshed import SupportsRichComparison, SupportsRichComparisonT as _T
+from _typeshed import SupportsRichComparison
 from collections.abc import Callable, Generator, Iterable
-from typing import Any, Final, TypeVar, overload
+from typing import Any, Final, TypeVar
 
 __all__ = ["heappush", "heappop", "heapify", "heapreplace", "merge", "nlargest", "nsmallest", "heappushpop"]
 
@@ -10,29 +10,13 @@ if sys.version_info >= (3, 14):
     # Added to __all__ in 3.14.1
     __all__ += ["heapify_max", "heappop_max", "heappush_max", "heappushpop_max", "heapreplace_max"]
 
-    def _heappush_max(heap: list[_T], item: _T, /) -> None: ...  # undocumented
-    def _heappushpop_max(heap: list[_T], item: _T, /) -> _T: ...  # undocumented
-
 _S = TypeVar("_S")
 
 __about__: Final[str]
 
-@overload
 def merge(
-    *iterables: Iterable[_S], key: Callable[[_S], SupportsRichComparison], reverse: bool = False
+    *iterables: Iterable[_S], key: Callable[[_S], SupportsRichComparison] | None = None, reverse: bool = False
 ) -> Generator[_S]: ...
-@overload
-def merge(
-    *iterables: Iterable[_T], key: None = None, reverse: bool = False
-) -> Generator[_T]: ...
-@overload
-def nlargest(n: int, iterable: Iterable[_S], key: Callable[[_S], SupportsRichComparison]) -> list[_S]: ...
-@overload
-def nlargest(n: int, iterable: Iterable[_T], key: None = None) -> list[_T]
-@overload
-def nsmallest(n: int, iterable: Iterable[_S], key: Callable[[_S], SupportsRichComparison]) -> list[_S]: ...
-@overload
-def nsmallest(n: int, iterable: Iterable[_T], key: None = None) -> list[_T]
-def _heapify_max(heap: list[SupportsRichComparison]) -> None: ...  # undocumented
-def _heappop_max(heap: list[_T], /) -> _T: ...  # undocumented
-def _heapreplace_max(heap: list[_T], item: _T, /) -> _T: ...  # undocumented
+def nlargest(n: int, iterable: Iterable[_S], key: Callable[[_S], SupportsRichComparison] | None = None) -> list[_S]: ...
+def nsmallest(n: int, iterable: Iterable[_S], key: Callable[[_S], SupportsRichComparison] | None = None) -> list[_S]: ...
+def _heapify_max(heap: list[Any], /) -> None: ...  # undocumented
